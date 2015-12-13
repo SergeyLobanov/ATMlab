6b467b7b17db785b32cf75082b8f42942405a697
SergeyLobanov <sinet_ua@bk.ru>
2015-12-13 19:53:04 +0200

    ATM 1.7

--- src/test/java/ua/pti/myatm/ATMTest.java
+++ src/test/java/ua/pti/myatm/ATMTest.java
@@ -7,7 +7,6 @@ package ua.pti.myatm;
 
 import org.junit.Test;
 import org.mockito.InOrder;
-import org.mockito.Mockito;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
@@ -75,15 +74,16 @@ public class ATMTest {
         int pinCode = 1111;
         when(card.checkPin(pinCode)).thenReturn(true);
         when(card.isBlocked()).thenReturn(false);
-        atm.validateCard(card, pinCode);
         Account acc = mock(Account.class);
         when(card.getAccount()).thenReturn(acc);
         double accBalance = 830;
         when(acc.getBalance()).thenReturn(accBalance);
-        assertEquals(accBalance, atm.checkBalance(), 0.0);
+        atm.validateCard(card, pinCode);
+        atm.checkBalance();
         InOrder inOrder = inOrder(card,acc);
         inOrder.verify(card).getAccount();
         inOrder.verify(acc).getBalance();
+        assertEquals(accBalance, atm.checkBalance(), 0.0);
     }
 
     @Test
@@ -103,16 +103,12 @@ public class ATMTest {
         double amount = 230;
         when(card.getAccount().withdrow(amount)).thenReturn(accBalance - amount);
         atm.getCash(amount);
+        InOrder inOrder = inOrder(card,acc);
+        inOrder.verify(card).getAccount();
+        inOrder.verify(acc, atLeastOnce()).getBalance();
+        inOrder.verify(acc, times(1)).withdrow(amount);
         when(acc.getBalance()).thenReturn(accBalance - amount);
-        assertEquals(atm.getMoneyInATM(), atmBalance - amount, 0.0);
-        assertEquals(atm.checkBalance(), accBalance - amount, 0.0);
         assertEquals(600, acc.getBalance(), 0.0);//we need obvious chek?
-//        InOrder inOrder = inOrder(card,acc);
-//        inOrder.verify(card).checkPin(pinCode);
-//        inOrder.verify(card).isBlocked();
-//        inOrder.verify(card).getAccount();
-//        inOrder.verify(acc, atLeastOnce()).getBalance();
-//        inOrder.verify(acc, times(1)).withdrow(amount);
     }
 
     @Test(expected = NotEnoughMoneyInATMException.class)
@@ -120,15 +116,18 @@ public class ATMTest {
         System.out.println("getCashForNotEnoughMoneyInATM");
         ATM atm = new ATM(1000);
         Card card = mock(Card.class);
-        Account acc = mock(Account.class);
         int pinCode = 1111;
         when(card.checkPin(pinCode)).thenReturn(true);
         when(card.isBlocked()).thenReturn(false);
         atm.validateCard(card, pinCode);
+        Account acc = mock(Account.class);
         when(card.getAccount()).thenReturn(acc);
+        double accBalance = 830;
+        when(acc.getBalance()).thenReturn(accBalance);
         atm.checkBalance();
         double amount = 2000;
         atm.getCash(amount);
+        assertEquals(accBalance, atm.getCash(amount),0.0);
     }
 
     @Test(expected = NotEnoughMoneyInAccountException.class)
@@ -145,10 +144,10 @@ public class ATMTest {
         double accBalance = 830;
         when(acc.getBalance()).thenReturn(accBalance);
         double amount = 1430;
-        when(card.getAccount().withdrow(amount)).thenReturn(amount);
         atm.getCash(amount);
         InOrder inOrder=inOrder(card,acc);
         inOrder.verify(card).getAccount();
         inOrder.verify(acc).getBalance();
+        assertEquals(amount, atm.getCash(amount), 0.0);
     }
 }
